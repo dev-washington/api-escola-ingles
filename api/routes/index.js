845e34858008b9984a31a64b909b8b901196b0d8
washington-wj <washingtonj047@gmail.com>
2022-02-21 16:56:23 -0300

    ORM-Sequelize and MySQL

---
+++ api/routes/index.js
@@ -0,0 +1,14 @@
+const bodyParse = require('body-parser')
+const pessoas = require('./PessoaRouter')
+const niveis = require('./NivelRouter')
+const turmas = require('./TurmaRouter')
+
+module.exports = app => {
+    app.use(
+      bodyParse.json(),
+      bodyParse.urlencoded({extended: false}),
+      pessoas,
+      niveis,
+      turmas
+    )
+}
