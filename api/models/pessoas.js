74bfa5e36f3baee7b157d36571eb03071c0d9764
washington-wj <washingtonj047@gmail.com>
2022-02-22 01:44:23 -0300

    feat: add validate email and name

--- api/models/pessoas.js
+++ api/models/pessoas.js
@@ -1,11 +1,36 @@
 'use strict'
 module.exports = (sequelize, DataTypes) => {
   const Pessoas = sequelize.define('Pessoas', {
-    nome: DataTypes.STRING,
+    nome: {
+      type: DataTypes.STRING,
+      validate: {
+        funcaoValidadora: function (dado) {
+          if (dado.length < 3) throw new Error('o campo nome deve ter mais de 3 caracteres')
+        }
+      }
+    },
     ativo: DataTypes.BOOLEAN,
-    email: DataTypes.STRING,
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: {
+          args: true,
+          msg: 'dados do tipo e-mail inválidos'
+        }
+      }
+    },
+
     role: DataTypes.STRING
-  }, { paranoid: true })
+  }, {
+    paranoid: true,
+    defaultScope: {
+      where: { ativo: true }
+    },
+    scopes: {
+      todos: { where: {} }
+      // etc: { constraint: valor }
+    }
+  })
   Pessoas.associate = function (models) {
     Pessoas.hasMany(models.Turmas, {
       foreignKey: 'docente_id'
