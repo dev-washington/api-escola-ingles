293dd781318b5af4bbfa0a29eaf12b61ac763d1c
washington-wj <washingtonj047@gmail.com>
2022-02-23 17:08:24 -0300

    feat: starting bridge

---
+++ api/services/index.js
@@ -0,0 +1,11 @@
+const PessoasServices = require('./PessoaService')
+const TurmasServices = require('./TurmaService')
+const NiveisServices = require('./NivelService')
+const MatriculasServices = require('./MatriculaService')
+
+module.exports = {
+  PessoasServices: PessoasServices,
+  TurmasServices: TurmasServices,
+  NiveisServices: NiveisServices,
+  MatriculasServices: MatriculasServices
+}
